3e4fe46265731ffce7a5e151fe6d0647e237f84d
kamilmaly <k.malysa94@gmail.com>
2024-01-29 12:02:36 +0100

    Add project files.

---
+++ Behavioral/Strategy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var strategy = new CarStrategy();
+
+            var map = new Map(strategy);
+
+            var start = new Coordinate();
+            var end = new Coordinate();
+
+            map.CreateRoute(start, end);
+        }
+    }
+}
---
+++ Creational/Singleton/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var cfg = Configuration.GetInstance();
+            var cfg2 = Configuration.GetInstance();
+
+            if (ReferenceEquals(cfg, cfg2))
+            {
+                Console.WriteLine("Configuration is a singleton");
+            }
+        }
+    }
+}
